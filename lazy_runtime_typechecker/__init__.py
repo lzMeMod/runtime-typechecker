d5b0eee98b9ea9400601ac8cf32040e6f3aaeb96
lzMeMod <luis.zeiler99@gmail.com>
2022-08-19 23:05:01 +0200

    Add pipeline for pull requests

--- lazy_runtime_typechecker/__init__.py
+++ lazy_runtime_typechecker/__init__.py
@@ -1,2 +1,9 @@
+"""
+Module to check methods at runtime for any type issues.
+"""
 from lazy_runtime_typechecker.typechecker import static_typed
-from lazy_runtime_typechecker.typechecker_errors import InputTypeingError, OutputTypeingError, TypeingError
+from lazy_runtime_typechecker.typechecker_errors import (
+    InputTypeingError,
+    OutputTypeingError,
+    TypeingError,
+)
